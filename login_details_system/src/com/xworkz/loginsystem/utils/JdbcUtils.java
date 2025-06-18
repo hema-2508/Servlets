6c5427b73d95527d911a14a17bdbd6353e803b84
hema-2508 <hema.acharya2508@gmail.com>
2025-06-18 16:38:00 +0530

    Working on JDBC connections

---
+++ login_details_system/src/com/xworkz/loginsystem/utils/JdbcUtils.java
@@ -0,0 +1,14 @@
+package com.xworkz.loginsystem.utils;
+
+public class JdbcUtils {
+
+    private JdbcUtils(){
+
+    }
+
+    public static final String jdbcUrl="jdbc:mysql://localhost:3306/weather";
+    public static final String jdbcUserName ="root";
+    public static final String jdbcPassword="Hema@342434";
+    public static final String jdbcDriver ="com.mysql.cj.jdbc.Driver";
+
+}
